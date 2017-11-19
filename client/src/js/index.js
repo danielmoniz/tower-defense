8b3c66b526777dd10d25b426a1c9d52799375a4b
Daniel Moniz <daniel.moniz@gmail.com>
2017-11-19 11:25:55 -0500

    Remove unnecessary imports from index.js .

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,9 +1,5 @@
 import { useStrict } from 'mobx'
 
-import Unit from 'Unit'
-import Cannon from 'Cannon'
-import Tank from 'Tank'
-import { GRID_SIZE } from 'appConstants'
 import Game from 'Game'
 
 useStrict(true)
