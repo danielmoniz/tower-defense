298b6927c6c2dd3b8957777ba91bf89cca80c04f
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-12 22:51:13 -0500

    Set up ServerGame with performance. Not sending yet.

--- src/js/game/ServerGame.js
+++ src/js/game/ServerGame.js
@@ -3,6 +3,9 @@ import { observable, computed, action, autorun } from 'mobx'
 
 import Game from './Game'
 import WaveSpawnerLocal from '../WaveSpawnerLocal'
+import Performance from '../Performance'
+import Cooldown from '../Cooldown'
+import { GAME_REFRESH_RATE } from '../appConstants'
 
 class ServerGame extends Game {
 
@@ -11,12 +14,36 @@ class ServerGame extends Game {
     this.runningOnServer = true
     this.serverInfo = serverInfo
     this.wave = new WaveSpawnerLocal(this.createEnemy.bind(this))
+
+    // for calculating performance
+    this.performance = new Performance(1000, GAME_REFRESH_RATE)
+
+    this.performanceCooldown = Cooldown.createTimeBased(2000, GAME_REFRESH_RATE, {
+      callback: this.sendPerformance.bind(this),
+      autoActivate: true,
+    })
   }
 
   spawnWave() {
     const newEnemies = super.spawnWave()
     this.emitter.spawnWave(this.serverInfo.gameNumber, newEnemies)
     return newEnemies
+  }
+
+  sendPerformance() {
+    // this.emitter.sendPerformance(this.performance.getAverage())
+    console.log('Should be sending performance instructions to all clients!');
+  }
+
+  // CALCULATE SERVER SPEED - can use to slow down game to keep it better synced
+  checkPerformance() {
+    this.performance.next()
+    this.performanceCooldown.tick()
+  }
+
+  gameLogic() {
+    super.gameLogic()
+    this.checkPerformance()
   }
 
 }
