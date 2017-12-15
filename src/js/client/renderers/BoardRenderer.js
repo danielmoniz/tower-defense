2fdc5645a15131c7a4e1994678dd9e9b0223e481
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-15 15:08:40 -0500

    Refactor GameRenderer into three files.

---
+++ src/js/client/renderers/BoardRenderer.js
@@ -0,0 +1,35 @@
+
+import { autorun } from 'mobx'
+
+import { GRID_SIZE } from '../../appConstants'
+
+export default class BoardRenderer {
+
+  setupGameBox(game) {
+    this.app = new PIXI.Application({
+      width: game.width,
+      height: game.height,
+      antialias: true,
+      transparent: false,
+      resolution: 1,
+    })
+    document.body.appendChild(this.app.view)
+    this.app.renderer.backgroundColor = 0xFFFFFF
+    this.app.renderer.view.style.border = '2px solid black'
+
+    this.setupCredits(game)
+
+    this.gameBox = document.querySelector("#display-box")
+    this.gameBox.style.width = game.width + 'px'
+    this.gameBox.style.height = game.height + 'px'
+    this.gameBoxBound = this.gameBox.getBoundingClientRect()
+  }
+
+  setupCredits(game) {
+    const creditsDisplay = document.querySelector(".remainingCredits")
+    autorun(() => {
+      creditsDisplay.innerHTML = game.credits.current
+    })
+  }
+
+}
--- src/js/client/renderers/GameRenderer.js
+++ src/js/client/renderers/GameRenderer.js
@@ -2,152 +2,27 @@
 import { autorun } from 'mobx'
 
 import { GRID_SIZE } from '../../appConstants'
+import BoardRenderer from './BoardRenderer'
+import GameEvents from './GameEvents'
 
 export default class GameRenderer {
   constructor(game) {
     this.game = game
-    this.setupGameBox()
-    this.addEventHandlers()
-    this.creditsDisplay = document.querySelector(".remainingCredits")
 
-    autorun(() => {
-      this.creditsDisplay.innerHTML = game.credits.current
-    })
+    this.board = new BoardRenderer()
+    this.events = new GameEvents()
+
+    this.createGameBoard()
+
+    this.setUpEvents() // relies on game board being set up
   }
 
-  setupGameBox() {
-    this.app = new PIXI.Application({
-      width: this.game.width,
-      height: this.game.height,
-      antialias: true,
-      transparent: false,
-      resolution: 1,
-    })
-    document.body.appendChild(this.app.view)
-    this.app.renderer.backgroundColor = 0xFFFFFF
-    this.app.renderer.view.style.border = '2px solid black'
-
-    this.gameBox = document.querySelector("#display-box")
-    this.gameBox.style.width = this.game.width + 'px'
-    this.gameBox.style.height = this.game.height + 'px'
-    this.gameBoxBound = this.gameBox.getBoundingClientRect()
-    this.gameCanvas = this.setupGameCanvas(this.gameBox, 'gameCanvas')
-    this.gameCanvasContext = this.gameCanvas.getContext('2d')
+  createGameBoard() {
+    this.board.setupGameBox(this.game)
   }
 
-  setupGameCanvas(frame, id, width = this.game.width, height = this.game.height) {
-    let canvas = document.createElement('canvas');
-    canvas.id = id
-    canvas.width = width
-    canvas.height = height
-    frame.append(canvas)
-    return canvas
-  }
-
-  addEventHandlers() {
-    this.addKeyPresses()
-    this.addPause()
-    this.addResume()
-    this.addPlaceTower()
-    this.addPlaceTowerFollow()
-    this.addPlacingTowerHide()
-    this.addPlacingTowerShow()
-    this.addPlaceTowerOnMap()
-    this.addSpawnWave()
-    this.addNewGame()
-  }
-
-  addNewGame() {
-    const newGameButton = document.querySelector("button#new-game")
-    newGameButton.addEventListener('click', () => {
-      this.game.newGame()
-    })
-  }
-
-  addPause() {
-    const pauseMoveButton = document.querySelector("button#pause")
-    pauseMoveButton.addEventListener('click', () => {
-      this.game.sendPause()
-    })
-  }
-
-  addResume() {
-    const resumeMoveButton = document.querySelector("button#resume")
-    resumeMoveButton.addEventListener('click', () => {
-      this.game.sendPlay()
-    })
-  }
-
-  addPlaceTower() {
-    const placeTowerButton = document.querySelector("button#place-tower")
-    placeTowerButton.addEventListener('click', () => {
-      this.game.selectNewCannon()
-    })
-  }
-
-  addPlaceTowerFollow() {
-    this.gameBox.addEventListener('mousemove', (event) => {
-      if (this.game.placingTower) {
-        const bound = this.gameBoxBound
-        const placingTower = this.game.placingTower
-
-        const actualX = event.offsetX - placingTower.width / 2.0 + (GRID_SIZE / 2)
-        const actualY = event.offsetY - placingTower.height / 2.0 + (GRID_SIZE / 2)
-        let gridX = Math.floor(actualX / GRID_SIZE) * GRID_SIZE
-        let gridY = Math.floor(actualY / GRID_SIZE) * GRID_SIZE
-
-        // prevent towers being placed over left/top edges
-        gridX = Math.max(gridX, 0)
-        gridY = Math.max(gridY, 0)
-
-        // prevent towers overlapping right/bottom edges
-        gridX = Math.min(gridX, this.game.width - placingTower.width)
-        gridY = Math.min(gridY, this.game.height - placingTower.height)
-
-        placingTower.jumpTo(gridX, gridY)
-      }
-    })
-  }
-
-  addPlacingTowerHide() {
-    this.gameBox.addEventListener('mouseleave', (event) => {
-      if (this.game.placingTower) {
-        this.game.placingTower.hide()
-      }
-    })
-  }
-
-  addPlacingTowerShow() {
-    this.gameBox.addEventListener('mouseenter', (event) => {
-      if (this.game.placingTower) {
-        this.game.placingTower.show()
-      }
-    })
-  }
-
-  addPlaceTowerOnMap() {
-    this.gameBox.addEventListener('click', (event) => {
-      this.game.sendPlaceTower()
-    })
-  }
-
-  addSpawnWave() {
-    const button = document.querySelector("button#spawn-wave")
-    button.addEventListener('click', (event) => {
-      this.game.spawnWaveEarly()
-    })
-  }
-
-  addKeyPresses() {
-    document.addEventListener('keydown', (event) => {
-      // console.log(event.key);
-      if (event.key === 'Escape') {
-        this.game.deselectAll()
-      } else if (event.key === ' ') {
-        event.preventDefault()
-        this.game.spawnWaveEarly()
-      }
-    })
+  setUpEvents() {
+    this.events.addEventHandlers(this.game, this.board.gameBox)
   }
 
 }
