3b0cc4b67ae7d7fa6c51aa1a0cb9eef7cac8510c
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-16 10:51:41 -0500

    Add nose to enemy units to see front.

--- src/js/client/renderers/PixiEnemyRenderer.js
+++ src/js/client/renderers/PixiEnemyRenderer.js
@@ -9,15 +9,29 @@ export default class PixiEnemyRenderer extends PixiUnitRenderer {
     const container = super.startRender(unit, board)
 
     const healthBarHeight = 10
+    const unitColour = 0x66CCFF
+    const outlineColour = 0xFF3300
 
-    let unitBase = new PIXI.Graphics()
-    unitBase.beginFill(0x66CCFF)
-    unitBase.lineStyle(4, 0xFF3300, 1);
+    const unitBase = new PIXI.Graphics()
+    unitBase.beginFill(unitColour)
+    unitBase.lineStyle(4, outlineColour, 1);
     unitBase.drawRect(0, 0, unit.width, unit.height);
     unitBase.endFill();
     container.addChild(unitBase)
 
-    let healthBar = new PIXI.Graphics()
+    const nose = new PIXI.Graphics()
+    nose.beginFill(unitColour)
+    nose.lineStyle(4, outlineColour, 1)
+    nose.drawPolygon([
+      0, 10,
+      0, unit.height - 10,
+      -(unit.width / 4), unit.height / 2,
+      0, 10,
+    ])
+    nose.endFill()
+    container.addChild(nose)
+
+    const healthBar = new PIXI.Graphics()
     healthBar.beginFill(0x40ef4c)
     healthBar.lineStyle(1, 0x000000, 1)
     healthBar.drawRect(0, unit.height - healthBarHeight, unit.width, healthBarHeight)
