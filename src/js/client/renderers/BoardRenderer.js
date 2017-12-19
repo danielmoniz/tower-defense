2b0141d0957d67b3c6bed14694ec62802489fb93
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-19 10:57:39 -0500

    Load enemy assets into Pixi texture cache.

--- src/js/client/renderers/BoardRenderer.js
+++ src/js/client/renderers/BoardRenderer.js
@@ -17,12 +17,32 @@ export default class BoardRenderer {
     this.app.renderer.backgroundColor = 0xFFFFFF
     this.app.renderer.view.style.border = '2px solid black'
 
+    this.loadUnitAssets()
+
     this.setupCredits(game)
 
     this.gameBox = document.querySelector("#display-box")
     this.gameBox.style.width = game.width + 'px'
     this.gameBox.style.height = game.height + 'px'
     this.gameBoxBound = this.gameBox.getBoundingClientRect()
+  }
+
+  loadUnitAssets() {
+    // @TODO Move this into another file
+    // load assets into PIXI
+    const loader = new PIXI.loaders.Loader()
+    loader.add('tank', '/images/tank.png')
+          .add('tank_normal', '/images/normal.png')
+          .add('tank_fast', '/images/fast.png')
+    console.log("Loading images...");
+    loader.on("progress", (loader, resource) => {
+      const completion = `${Math.floor(loader.progress)}%`
+      console.log(completion);
+    })
+    loader.load((loader, resources) => {
+      console.log("All images loaded!");
+      // console.log(loader);
+    })
   }
 
   setupCredits(game) {
