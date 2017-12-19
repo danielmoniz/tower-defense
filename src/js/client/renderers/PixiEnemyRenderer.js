ee467518256eec5d2b57fb3c22c1c15d4c63a67b
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-19 11:16:16 -0500

    Update Pixi enemy images as top-down tank.

--- src/js/client/renderers/PixiEnemyRenderer.js
+++ src/js/client/renderers/PixiEnemyRenderer.js
@@ -8,28 +8,14 @@ export default class PixiEnemyRenderer extends PixiUnitRenderer {
   startRender(unit, board) {
     const container = super.startRender(unit, board)
 
-    const healthBarHeight = 10
+    const healthBarHeight = 6
     const unitColour = 0x66CCFF
     const outlineColour = 0xFF3300
 
-    const unitBase = new PIXI.Graphics()
-    unitBase.beginFill(unitColour)
-    unitBase.lineStyle(4, outlineColour, 1);
-    unitBase.drawRect(0, 0, unit.width, unit.height);
-    unitBase.endFill();
+    const unitBase = new PIXI.Sprite(PIXI.utils.TextureCache["tank"])
+    unitBase.width = unit.width
+    unitBase.height = unit.height
     container.addChild(unitBase)
-
-    const nose = new PIXI.Graphics()
-    nose.beginFill(unitColour)
-    nose.lineStyle(4, outlineColour, 1)
-    nose.drawPolygon([
-      0, 10,
-      0, unit.height - 10,
-      -(unit.width / 4), unit.height / 2,
-      0, 10,
-    ])
-    nose.endFill()
-    container.addChild(nose)
 
     const healthBar = new PIXI.Graphics()
     healthBar.beginFill(0x40ef4c)
