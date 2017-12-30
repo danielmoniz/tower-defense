e1c520e1bddb1b0d2e006b5ac56d2307899a2378
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-30 09:40:31 -0500

    Refactor Performance.js into smaller methods.

--- src/js/Performance.js
+++ src/js/Performance.js
@@ -17,51 +17,70 @@ class Performance {
     this.marks = []
   }
 
+  incrementTicks(ticks) {
+    return ticks.map((numTicks) => {
+      return numTicks + 1
+    })
+  }
+
   // @TODO Refactor this function into smaller pieces.
   next() {
     if (this.pauseTime) { return }
 
-    const now = Date.now()
     this.cooldown.tick()
-    this.ticks = this.ticks.map((numTicks) => {
-      return numTicks + 1
-    })
+    this.ticks = this.incrementTicks(this.ticks)
 
+    this.sample()
+  }
+
+  sample() {
     if (!this.cooldown.ready()) { return }
 
     this.cooldown.activate()
 
-    // calculate time, but subtract the total paused time.
-    const adjustedTime = now - this.totalPausedTime
-    this.times.push(adjustedTime)
-    this.ticks.push(0)
+    this.addNewTime(this.times)
+    this.addNewTick(this.ticks)
+    this.sampleMark()
 
+    this.trimSamples()
+  }
+
+  sampleMark() {
     if (this.times.length <= 1) { return }
+    const newMark = this.getNewMark(this.tickLength, this.times, this.ticks)
+    this.marks.push(newMark)
+    // console.log('Latest mark:', this.marks[this.marks.length - 1]);
+  }
 
-    const numData = this.times.length
-    const latestTimePassed = this.times[numData - 1] - this.times[numData - 2]
-    const latestTicksPassed = this.ticks[numData - 2]
-    const timePerTick = latestTimePassed / latestTicksPassed
-    const performance = this.tickLength / timePerTick
-    this.marks.push(performance)
-    // console.log('-----');
-    // console.log(this.marks);
+  // calculate time, but subtract the total paused time.
+  addNewTime(times) {
+    const adjustedTime = Date.now() - this.totalPausedTime
+    times.push(adjustedTime)
+  }
 
-    // console.log(this.getAverage());
+  addNewTick(ticks) {
+    ticks.push(0)
+  }
 
+  trimSamples() {
     if (this.times.length > this.maxTimePoints) {
       this.times.splice(0, 1)
       this.ticks.splice(0, 1)
       this.marks.splice(0, 1)
     }
-
-    // console.log(this.marks);
-    // console.log('Latest mark:', this.marks[this.marks.length - 1]);
   }
 
-  getAverage() {
-    if (this.marks.length < 2) { return 1 }
-    const currentAverage = this.marks.length / this.marks.reduce((memo, mark) => {
+  getNewMark(tickLength, times, ticks) {
+    const numData = times.length
+    const latestTimePassed = times[numData - 1] - times[numData - 2]
+    const latestTicksPassed = ticks[numData - 2]
+    const timePerTick = latestTimePassed / latestTicksPassed
+    return tickLength / timePerTick // @TODO This should be flipped! Use inverse instead.
+  }
+
+  getAverage(marks) {
+    if (marks.length < 2) { return 1 }
+    const currentAverage = marks.length / marks.reduce((memo, mark) => {
       return memo + mark
     })
     // console.log("Average over last " + this.marks.length + " data points:", currentAverage);
@@ -69,7 +88,7 @@ class Performance {
   }
 
   getSpeedSuggestion() {
-    const average = this.getAverage()
+    const average = this.getAverage(this.marks)
     const difference = Math.max(1, average) - 1
     return 1 + (difference * 0.9)
   }
