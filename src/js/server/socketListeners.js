2c753de061d15c9bede91b35bdb5519efc140017
Daniel Moniz <daniel.moniz@gmail.com>
2017-12-30 09:39:05 -0500

    Only allow some server actions if a game exists.

--- src/js/server/socketListeners.js
+++ src/js/server/socketListeners.js
@@ -2,28 +2,37 @@
 export default function socketListeners(socket, emitter, serverFunctions) {
   socket.on('place tower', (tower) => {
     console.log('placing tower at:', tower.x, tower.y);
-    socket.gameManager.game.placeTower(tower)
-    socket.broadcast.to(socket.roomId).emit('place tower', tower)
+    if (socket.gameManager && socket.gameManager.game) {
+      socket.gameManager.game.placeTower(tower)
+      socket.broadcast.to(socket.roomId).emit('place tower', tower)
+    }
   })
 
   socket.on('spawn wave early', () => {
-    const newEnemies = socket.gameManager.game.spawnWave()
-    emitter.spawnWave(socket.roomId, newEnemies)
+    if (socket.gameManager && socket.gameManager.game) {
+      const newEnemies = socket.gameManager.game.spawnWave()
+      emitter.spawnWave(socket.roomId, newEnemies)
+    }
   })
 
   socket.on('pause', () => {
     console.log('pausing');
-    socket.gameManager.game.pause()
-    emitter.pause(socket)
+    if (socket.gameManager && socket.gameManager.game) {
+      socket.gameManager.game.pause()
+      emitter.pause(socket)
+    }
   })
 
   socket.on('play', () => {
     console.log('playing');
-    socket.gameManager.game.play()
-    emitter.play(socket)
+    if (socket.gameManager && socket.gameManager.game) {
+      socket.gameManager.game.play()
+      emitter.play(socket)
+    }
   })
 
   socket.on('new game', (gameNumber) => {
+    // @TODO Handle case where there is no GameManager? When would this happen?
     socket.gameManager.start()
     // @TODO should only go to this game/room specifically
     emitter.startGame(gameNumber)
