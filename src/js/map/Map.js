361bfc494c9dba4307cd4b27053aca89bdd6a879
itsamiga <jasper.g.huang@gmail.com>
2018-01-08 11:24:11 -0500

    getPath partial functionality
    - less desirable than searching step-by-step as needed, per unit
      - changing map layout using getPath would require recalculation of path array for all units

--- src/js/map/Map.js
+++ src/js/map/Map.js
@@ -8,11 +8,12 @@ export default class Map {
     this.setupWeights()
     this.setupPathLengths()
 
-    this.randomizeWeights(0.3)
+    this.randomizeWeights(0.35)
     this.calculatePathLengths()
 
     console.log(this.weights);
     console.log(this.pathLengths);
+    console.log(this.getPath());
   }
 
   randomizeWeights(wallProbability = 0.1) {
@@ -33,63 +34,126 @@ export default class Map {
 
     this.setupPathLengths()
 
-    if (!this.coordinateIsValid(endX, endY) || !this.weights[endX][endY]) {
+    if (!this.positionIsValid(endX, endY) || !this.weights[endX][endY]) {
       return
     }
     this.pathLengths[endX][endY] = 0
     let queue = []
     let currentPos = { x: endX, y: endY }
     let lastPos = {}
-    this.searchDirections(queue, currentPos)
+    this.searchAccessibleDirections(queue, currentPos)
 
     while (queue.length != 0) {
       currentPos = queue.shift()
-      this.searchDirections(queue, currentPos)
+      this.searchAccessibleDirections(queue, currentPos)
     }
 
     console.log(new Date() - start)
   }
 
-  searchDirections(queue, currentPos) {
+  searchAccessibleDirections(queue, currentPos) {
     let north = { x: currentPos.x, y: currentPos.y - 1 }
     let south = { x: currentPos.x, y: currentPos.y + 1 }
     let west = { x: currentPos.x - 1, y: currentPos.y }
     let east = { x: currentPos.x + 1, y: currentPos.y }
 
-    if (this.coordinateIsValid(north.x, north.y) &&
+    if (this.positionIsValid(north.x, north.y) &&
         this.pathLengths[north.x][north.y] == null) { // north
       this.addToQueue(queue, north, currentPos)
     }
-    if (this.coordinateIsValid(south.x, south.y) &&
+    if (this.positionIsValid(south.x, south.y) &&
         this.pathLengths[south.x][south.y] == null) { // south
       this.addToQueue(queue, south, currentPos)
     }
-    if (this.coordinateIsValid(west.x, west.y) &&
+    if (this.positionIsValid(west.x, west.y) &&
         this.pathLengths[west.x][west.y] == null) { // west
       this.addToQueue(queue, west, currentPos)
     }
-    if (this.coordinateIsValid(east.x, east.y) &&
+    if (this.positionIsValid(east.x, east.y) &&
         this.pathLengths[east.x][east.y] == null) { // east
       this.addToQueue(queue, east, currentPos)
     }
   }
 
-  addToQueue(queue, coordinate, currentPos) {
-    let newWeight = this.weights[coordinate.x][coordinate.y]
+  addToQueue(queue, newPos, currentPos) {
+    let newWeight = this.weights[newPos.x][newPos.y]
     if (newWeight == 0) {
-      this.pathLengths[coordinate.x][coordinate.y] = -1
+      this.pathLengths[newPos.x][newPos.y] = -1
     } else {
       let newLength = this.pathLengths[currentPos.x][currentPos.y] + newWeight
-      queue.push(coordinate)
-      this.pathLengths[coordinate.x][coordinate.y] = newLength
+      queue.push(newPos)
+      this.pathLengths[newPos.x][newPos.y] = newLength
     }
   }
 
-  coordinateIsValid(x, y) {
+  positionIsValid(x, y) {
     if (x < 0 || x >= this.tilesWide || y < 0 || y >= this.tilesHigh) {
       return false
     } else {
       return true
+    }
+  }
+
+  getPath(position = { x: 0, y: 0 }) {
+    let start = new Date()
+
+    let currentPos = position
+    let currentLength = this.pathLengths[currentPos.x][currentPos.y]
+    let path = []
+    path.push([currentPos.x, currentPos.y])
+
+    if (!this.positionIsAccessible(currentPos.x, currentPos.y)) {
+      return false
+    } else {
+      while (currentLength > 0) {
+        currentPos = this.searchPathDirections(path, currentPos, currentLength)
+        currentLength = this.pathLengths[currentPos.x][currentPos.y]
+      }
+    }
+
+    console.log(new Date() - start)
+    return path
+  }
+
+  searchPathDirections(path, currentPos, currentLength) {
+    let north = { x: currentPos.x, y: currentPos.y - 1 }
+    let south = { x: currentPos.x, y: currentPos.y + 1 }
+    let west = { x: currentPos.x - 1, y: currentPos.y }
+    let east = { x: currentPos.x + 1, y: currentPos.y }
+
+    let northLength = this.positionIsAccessible(north.x, north.y)
+    let southLength = this.positionIsAccessible(south.x, south.y)
+    let westLength = this.positionIsAccessible(west.x, west.y)
+    let eastLength = this.positionIsAccessible(east.x, east.y)
+
+    if (northLength !== false && northLength < currentLength) { // north
+      path.push([north.x, north.y])
+      return north
+    }
+    if (southLength !== false && southLength < currentLength) { // south
+      path.push([south.x, south.y])
+      return south
+    }
+    if (westLength !== false && westLength < currentLength) { // west
+      path.push([west.x, west.y])
+      return west
+    }
+    if (eastLength !== false && eastLength < currentLength) { // east
+      path.push([east.x, east.y])
+      return east
+    }
+  }
+
+  positionIsAccessible(x, y) {
+    if (!this.positionIsValid(x, y)) {
+      return false
+    }
+
+    let pathLength = this.pathLengths[x][y]
+    if (pathLength == null || pathLength < 0) {
+      return false
+    } else {
+      return pathLength
     }
   }
 
