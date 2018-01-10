7dcb2266629e67b15ee7a209c10c95221033a4bb
Daniel Moniz <daniel.moniz@gmail.com>
2018-01-10 12:42:45 -0500

    Add getDirectionFromNode() to Map.js . Needs refactor.

--- src/js/map/Map.js
+++ src/js/map/Map.js
@@ -12,7 +12,45 @@ export default class Map {
     this.calculatePathLengths()
 
     // console.log(this.weights);
-    // console.log(this.pathLengths);
+    console.log(this.pathLengths);
+  }
+
+  getDirectionFromNode(x, y) {
+    const north = this.pathLengths[x][y + 1] || -1
+    const south = this.pathLengths[x][y - 1] || -1
+    const east = this.pathLengths[x + 1] && this.pathLengths[x + 1][y] || -1
+    const west = this.pathLengths[x - 1] && this.pathLengths[x - 1][y] || -1
+    let directions = [
+      {direction: 'north', value: north, angle: this.degreesToRadians(90)},
+      {direction: 'south', value: south, angle: this.degreesToRadians(270)},
+      {direction: 'east', value: east, angle: this.degreesToRadians(0)},
+      {direction: 'west', value: west, angle: this.degreesToRadians(180)},
+    ]
+    const directionValues = directions.map((directionInfo) => {
+      return directionInfo.value
+    }).filter((value) => {
+      return value >= 0
+    })
+    // Return false if there is nowhere to go
+      // ^^^ Shouldn't happen if we prevent towers from being placed anywhere!
+      // however, terrain might still cause issues
+    if (directionValues.length === 0) { return false }
+
+    // @TODO What to do if on final space? ie. value of current value is 0?
+    const smallestValue = Math.min(...directionValues)
+    directions = directions.filter((direction) => {
+      return direction.value === smallestValue
+    })
+    console.log(directions);
+
+    // pick random direction out of smallest options (might be multiple)
+    const finalDirection = directions[Math.floor(Math.random() * directions.length)]
+    console.log(finalDirection);
+    return finalDirection.angle
+  }
+
+  degreesToRadians(degrees) {
+    return degrees * Math.PI / 180
   }
 
   randomizeWeights(wallProbability = 0.1) {
