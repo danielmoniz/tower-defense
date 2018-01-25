824bdc0c8f9937352058ca0a8ac8b64a4a6b96d0
Daniel Moniz <daniel.moniz@gmail.com>
2018-01-25 10:10:07 -0500

    Use CannonRenderer. Set up inherited options.

--- src/js/client/renderers/towers/CannonRenderer.js
+++ src/js/client/renderers/towers/CannonRenderer.js
@@ -6,4 +6,40 @@ import TowerRenderer from './TowerRenderer'
 
 export default class CannonRenderer extends TowerRenderer {
 
+  constructor(...args) {
+    super(...args)
+
+    this.towerBaseOptions = {
+      color: 0x66CCFF,
+      lineStyle: { width: 2, color: 0x000000, alpha: 1, },
+    }
+
+  }
+
+  startRender(unit, board) {
+    // super.startRender(unit, board)
+
+    const circleRadius = unit.width / 2
+    const gunOptions = this.getGunOptions(unit)
+
+    const { container, unitContainer } = this.getContainer(unit, board)
+    const { disableBackground, background } = this.setBackground(unit, unitContainer, this.backgroundOptions)
+    this.setTowerBase(unitContainer, circleRadius, this.towerBaseOptions)
+    const gunContainer = this.setGun(unit, unitContainer, gunOptions.gunHeight, gunOptions.gunLength, gunOptions)
+    const maxRange = this.setMaxRange(unit, container, circleRadius)
+    board.app.stage.addChild(container)
+
+    this.setAutorun(unit, background, disableBackground, unitContainer, gunContainer, maxRange)
+
+    return container
+  }
+
+  getGunOptions(unit) {
+    return {
+      color: 0x666666,
+      lineStyle: { width: 1, color: 0x000000, alpha: 1, },
+      gunHeight: 8,
+      gunLength: unit.width * 0.6,
+    }
+  }
 }
--- src/js/client/renderers/towers/TowerRenderer.js
+++ src/js/client/renderers/towers/TowerRenderer.js
@@ -6,54 +6,42 @@ import UnitRenderer from '../UnitRenderer'
 
 export default class TowerRenderer extends UnitRenderer {
 
-  startRender(unit, board) {
-    const circleRadius = unit.width / 2
-    const gunHeight = 8
-    const gunLength = unit.width * 0.6
+  constructor(...args) {
+    super(...args)
 
-    const backgroundOptions = {
-      backgroundColor: 0xCCCCCC,
-      disableBackgroundColor: 0xFF4444,
-      lineStyle: { width: 1, color: 0x000000, alpha: 0.5, },
-    }
-    const towerBaseOptions = {
-      color: 0x66CCFF,
-      lineStyle: { width: 2, color: 0x000000, alpha: 1, },
-    }
-    const gunOptions = {
-      color: 0x666666,
-      lineStyle: { width: 1, color: 0x000000, alpha: 1, },
-    }
-    const maxRangeOptions = {
+    this.maxRangeOptions = {
       color: 0x40ef4c,
       alpha: 0.2,
       lineStyle: { width: 3, color: 0x000000, alpha: 1, },
     }
+    this.backgroundOptions = {
+      backgroundColor: 0xCCCCCC,
+      disableBackgroundColor: 0xFF4444,
+      lineStyle: { width: 1, color: 0x000000, alpha: 0.5, },
+    }
+  }
 
+  startRender(unit, board) {
+    // @TODO do everything that is shared between all towers
     const { container, unitContainer } = this.getContainer(unit, board)
-    const { disableBackground, background } = this.setBackground(unit, unitContainer, backgroundOptions)
-    this.setTowerBase(unitContainer, circleRadius, towerBaseOptions)
-    const gunContainer = this.setGun(unit, unitContainer, gunHeight, gunLength, gunOptions)
-    const maxRange = this.setMaxRange(unit, container, circleRadius, maxRangeOptions)
+
+    const circleRadius = unit.width / 2
+
+    // set autorun for draw/redraw of maxRange
+
+    const maxRange = this.setMaxRange(unit, container, circleRadius)
+
+    // set autorun for turn turret
+
+    // set autorun for disable
+
+    // set autorun for ghosting
+
+    // render to board
     board.app.stage.addChild(container)
 
-    autorun(() => {
-      disable(unit, background, disableBackground, maxRange)
-    })
-
-    autorun(() => {
-      rotateToTarget(unit, gunContainer)
-    })
-
-    autorun(() => {
-      displayRange(unit, maxRange)
-    })
-
-    autorun(() => {
-      ghostUnit(unit, unitContainer)
-    })
-
-    return container
+    // return artifacts that might be useful
+    return { container, unitContainer, maxRange }
   }
 
   getContainer(unit, board) {
@@ -116,17 +104,23 @@ export default class TowerRenderer extends UnitRenderer {
     return gunContainer
   }
 
-  setMaxRange(unit, container, circleRadius, options) {
+  setMaxRange(unit, container, circleRadius) {
     const maxRange = new PIXI.Graphics()
-    maxRange.beginFill(options.color)
-    maxRange.lineStyle(options.lineStyle.width, options.lineStyle.color,
-      options.lineStyle.alpha)
-    maxRange.drawCircle(circleRadius, circleRadius, unit.range.current)
-    maxRange.endFill()
-    maxRange.alpha = options.alpha
-    container.addChildAt(maxRange, 0) // add to overall container, not to unit
-
+    this.drawMaxRange(maxRange, unit, circleRadius)
+    container.addChildAt(maxRange, 0) // add to bottom of container
     return maxRange
+  }
+
+  drawMaxRange(graphics, unit, circleRadius) {
+    const options = this.maxRangeOptions
+
+    graphics.clear()
+    graphics.beginFill(options.color)
+    graphics.lineStyle(options.lineStyle.width, options.lineStyle.color,
+      options.lineStyle.alpha)
+    graphics.drawCircle(circleRadius, circleRadius, unit.range.current)
+    graphics.endFill()
+    graphics.alpha = options.alpha
   }
 
   setAutorun(unit, background, disableBackground, unitContainer, gunContainer, maxRange) {
