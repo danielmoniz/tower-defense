c06aaff668d4e1100358667ddc6b52c180071cc0
Daniel Moniz <daniel.moniz@gmail.com>
2018-02-18 23:38:40 -0500

    Put the backgroundLayer on top of the mapLayer.

--- src/js/client/renderers/BoardRenderer.js
+++ src/js/client/renderers/BoardRenderer.js
@@ -26,13 +26,13 @@ export default class BoardRenderer {
 
     this.app.stage = new PIXI.display.Stage(); // necessary for layers to work
 
-    this.backgroundLayer = new PIXI.display.Layer()
     this.mapLayer = new PIXI.display.Layer()
+    this.backgroundLayer = new PIXI.display.Layer()
     this.unitsLayer = new PIXI.display.Layer()
     this.menuLayer = new PIXI.display.Layer()
 
-    this.app.stage.addChild(this.backgroundLayer)
     this.app.stage.addChild(this.mapLayer)
+    this.app.stage.addChild(this.backgroundLayer)
     this.app.stage.addChild(this.unitsLayer)
     this.app.stage.addChild(this.menuLayer)
 
