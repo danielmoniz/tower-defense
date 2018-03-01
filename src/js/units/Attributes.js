96f02bda6e87159ea6ba340f1881697bdb5a51b9
Daniel Moniz <daniel.moniz@gmail.com>
2018-03-01 14:45:20 -0500

    Update attributes.multiply and add tests.

--- src/js/units/Attributes.js
+++ src/js/units/Attributes.js
@@ -8,7 +8,7 @@ export const attributes = [
   },
   {
     name: 'Super speedy',
-    speed: multiply(normalFactor, 2),
+    speed: multiplyMultiplier(normalFactor, 2),
   },
   {
     name: 'Beefy',
@@ -21,14 +21,18 @@ export const attributes = [
   }
 ]
 
-export function getAttributeNames(attributes) {
-  return attributes.map((attribute) => attribute.name)
-}
-
 /*
  * Takes a multiplier (such as 1.2) and multiplies it without its base of 1.
- * Eg. multiply(1.2, 4) returns 1.8, not 4.8
+ * Eg. multiplyMultiplier(1.2, 4) returns 1.8, not 4.8
  */
-function multiply(number, factor) {
+export function multiplyMultiplier(number, factor) {
+  if (number < 0) {
+    throw 'Cannot handle multipliers less than zero.'
+  }
+  if (number === 0) { return 0 }
+
+  if (number < 1) {
+    return number * factor
+  }
   return (number - 1) * factor + 1
 }
