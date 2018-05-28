059844a997cb8fcd06b53fbd6f106cde1daa5a9b
Daniel Moniz <daniel.moniz@gmail.com>
2018-05-28 06:15:10 -0400

    Refactor ClientGame.updateBurningCooldown() .

--- src/js/game/ClientGame.js
+++ src/js/game/ClientGame.js
@@ -223,15 +223,11 @@ class ClientGame extends Game {
    * Updates the burning cooldown for a unit.
    */
   updateBurningCooldown(unit, data) {
-    // @TODO Refactor this.
     if (!data.burningInfo.cooldown) { return }
-
-    if (unit.burningInfo.cooldown) {
-      unit.burningInfo.cooldown.setTicksPassed(data.burningInfo.cooldown.ticksPassed)
-    } else {
+    if (!unit.burningInfo.cooldown) {
       unit.setBurningCooldown(data.burningInfo.cooldown.cooldownLength)
-      unit.burningInfo.cooldown.setTicksPassed(data.burningInfo.cooldown.ticksPassed)
     }
+    unit.burningInfo.cooldown.setTicksPassed(data.burningInfo.cooldown.ticksPassed)
   }
 
   setTowerTarget(tower) {
